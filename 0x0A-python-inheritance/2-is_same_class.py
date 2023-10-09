6bd915982b1dd943a0e52e8cc844b85748710b9b
mkinfe <mkinfe818@gmail.com>
2023-10-09 10:18:21 -0700

    inherit from

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
+
+
 def is_same_class(obj, a_class):
-    """ Function that returns True/False if obj is a type of a_class
-
-    Args:
-        obj: object
-        a_class: class type
-
-    Returns:
-        True if type of obj is a_class
-        False, otherwise
+    """'''function: is_same_class
+    obj: an object
+    a_class: a class
+    Returns: Bool
     """
-    return type(obj) is a_class
+    return type(obj) == a_class
+
